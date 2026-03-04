307f69b5d4ccf464fe7fb5d8df9002186572c923
SAHAYAASHIKLIBU <ashiklibu11@gmail.com>
2026-03-04 12:25:18 +0530

    copy pasted karapathy engine

--- engine.py
+++ engine.py
@@ -1,85 +1,94 @@
+
 class Value:
-    def __init__(self, data, prev = ()):
+    """ stores a single scalar value and its gradient """
+
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
-        self.prev = set(prev)
         self.grad = 0
+        # internal variables used for autograd graph construction
         self._backward = lambda: None
-    
-    def __repr__(self):
-        return f"Class Value, data = {self.data}"
+        self._prev = set(_children)
+        self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
     def __add__(self, other):
-        assert isinstance(other, (int, float, Value)), "only support int and float"
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other))
+        out = Value(self.data + other.data, (self, other), '+')
 
-        def backward():
+        def _backward():
             self.grad += out.grad
             other.grad += out.grad
-        out._backward = backward
+        out._backward = _backward
 
         return out
-    
-    def __mul__(self, other):
-        assert isinstance(other, (int, float, Value)), "only support int and float"
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other))
 
-        def backward():
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
-        out._backward = backward
+        out._backward = _backward
 
         return out
-    def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,))
 
-        def backward():
-            self.grad += 0 if self.data < 0 else 1 * out.grad
-        out._backward = backward
-        return out
     def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only support int and float"
-        # other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data ** other, (self,))
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
 
-        def backward():
-            self.grad += (other * (self.data**(other-1))) * out.grad
-        out._backward = backward
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
 
         return out
-    
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
     def backward(self):
-        self.grad = 1
+
+        # topological order all of the children in the graph
         topo = []
         visited = set()
-        def build(v):
+        def build_topo(v):
             if v not in visited:
                 visited.add(v)
-                for child in v.prev:
-                    build(child)
+                for child in v._prev:
+                    build_topo(child)
                 topo.append(v)
-        build(self)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
         for v in reversed(topo):
             v._backward()
-    
-    def __neg__(self):
-        return self.__mul__(-1)
-    
-    def __truediv__(self, other):
-        return self.__mul__(other**-1)
 
-    def __sub__(self, other):
-        return self.__add__(-other)
-    def __rsub__(self, other):
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
         return other + (-self)
-    
-    def __radd__(self, other):
-        return self.__add__(other)
-    def __rmul__(self, other):
-        return self * (other ** -1)
-    def __rtruediv__(self, other):
-        return other * (self ** -1)
 
+    def __rmul__(self, other): # other * self
+        return self * other
 
+    def __truediv__(self, other): # self / other
+        return self * other**-1
 
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
